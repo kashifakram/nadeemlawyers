afb8e5e36cc702f99f4029e10a345512d35a0615
Kashif Akram <k.akram@outlook.cm>
2021-06-01 15:31:50 +1000

    clean repo

--- _nuxt/static/1621933398/index_bk/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/index_bk", {data:[{}],fetch:{},mutations:void 0});
--- _nuxt/static/1621933398/index_bk/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621933398",layout:"default",error:a,serverRendered:true,routePath:"\u002Findex_bk",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
